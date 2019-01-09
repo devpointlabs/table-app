352d2ca40a65647f53aaa7e9c22b01181155a106
ian <clarksware24@gmail.com>
2019-01-09 15:47:44 -0700

    add dropdown to menu

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,34 +1,70 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { AuthConsumer, } from "../providers/AuthProvider";
 import { Link, withRouter, } from 'react-router-dom'
+import { Dropdown, Menu } from 'semantic-ui-react';
+
 import '../App.css'
 
 class Navbar extends React.Component {
 
-  rightNavItems = () => {
-    const { auth: { user, handleLogout, }, location, } = this.props;
+  admin= () => {
+    const {user} = this.props.auth
+    if (user.admin) {
+      return(
+      <Dropdown.Item>
+        <Link to = ''>Admin</Link>
+      </Dropdown.Item>
+      )
+    }
+  }
+
+  leftNavItems = () => {
+    const { user, handleLogout, } = this.props.auth;
 
     if (user) {
       return (
-        <Link to = '' class = 'navLink' onClick = {() => handleLogout(this.props.history)}>LOGOUT</Link>
+          <Dropdown item text={user.email}>
+            <Dropdown.Menu>
+              <Dropdown.Item>
+                <Link to = ''>DASHBOARD</Link>
+              </Dropdown.Item>
+                {this.admin()}
+              <Dropdown.Item>
+                <Link to = '' onClick = {() => handleLogout(this.props.history)}>LOGOUT</Link>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
       )
     } else {
       return (
-        <Fragment>
+        <>
+          <Menu.Item link>
           <Link to = '/login' class = 'navLink'>LOGIN</Link>
+          </Menu.Item>
+          <Menu.Item link>
           <Link to = '/register' class = 'navLink'>REGISTER</Link>
-        </Fragment>
+          </Menu.Item>
+        </>
       )
     }
   }
 
   render() {
     return (
-      <div class = 'navBar'>      
-        <Link to = '/' class = 'navLink'>WORK</Link>
-        <Link to = '/' class = 'navLink'>PAGES</Link>
-        <Link to = '/' class = 'active navLink'>HOME</Link>
-      </div>
+      <Menu borderless >
+        { this.leftNavItems() }
+        <Menu.Menu position="right">
+          <Menu.Item link>
+            <Link to = '/' class = 'navLink'>WORK</Link>
+          </Menu.Item>
+          <Menu.Item link>
+            <Link to = '/' class = 'navLink'>PAGES</Link>
+          </Menu.Item>
+          <Menu.Item link>
+            <Link to = '/' class = 'navLink'>HOME</Link>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
     )
   }
 }
