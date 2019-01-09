b0235126b1e50dd2dbb20c16a384b098844f1099
Maris Herrera <maris.lizete.herrera@gmail.com>
2019-01-09 15:28:13 -0700

    create table model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_161657) do
+ActiveRecord::Schema.define(version: 2019_01_09_222702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tables", force: :cascade do |t|
+    t.string "name"
+    t.integer "table_group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -39,7 +46,7 @@ ActiveRecord::Schema.define(version: 2019_01_09_161657) do
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
