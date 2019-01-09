90a752778a8e1ac077d57020da7567528f627074
Matt Westover <matthew.westover@gmail.com>
2019-01-09 15:36:21 -0700

    Event Controller actions completed

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,13 +1,42 @@
 class Api::EventsController < ApplicationController
+  before_action :set_event, only: [:show, :update, :destroy]
+
   def index
+    render json: Event.all.order(event_date: :desc)
   end
 
   def show
+    render json: @event
   end
 
-  def new
+  def create
+    event = Event.create(event_params)
+    if event.save
+      render json: event
+    else
+      render json: blog.errors
+    end
   end
 
-  def edit
+  def update
+    if @event.update(event_params)
+      render json: @event
+    else
+      render json: @event.errors
+    end
   end
+
+  def destroy
+    @event.destroy
+  end
+
+  private
+    def set_event
+      @event = Event.find(params[:id])
+    end
+
+    def event_params
+      params.require(:event).permit(:host, :image_url, :event_date, :dress_code)
+    end
+
 end
