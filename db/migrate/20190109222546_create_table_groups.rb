77f327ac678ae8ea9b9956876e4f8dd12580e4ee
Colton Davis <colt822lavay@outlook.com>
2019-01-09 15:45:41 -0700

    Add default: 15 to migration file

--- db/migrate/20190109222546_create_table_groups.rb
+++ db/migrate/20190109222546_create_table_groups.rb
@@ -3,7 +3,7 @@ class CreateTableGroups < ActiveRecord::Migration[5.2]
     create_table :table_groups do |t|
       t.string :name
       t.text :description
-      t.integer :max_seats
+      t.integer :max_seats, default: 15
 
       t.timestamps
     end
