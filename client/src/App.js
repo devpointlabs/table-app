5e699ea0a97907e4ed0635b76d1449be287485eb
Maris Herrera <maris.lizete.herrera@gmail.com>
2019-01-11 16:03:41 -0700

    add admin dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import AdminRoute from './components/AdminRoute';
 import { Container, } from "semantic-ui-react";
 import { Route, Switch, } from "react-router-dom";
 import Ticketing from './components/Ticketing';
+import AdminDashboard from './components/AdminDashboard';
 
 const App = () => (
   <Fragment>
@@ -20,6 +21,7 @@ const App = () => (
     <FetchUser>
       {/* <Container> */}
         <Switch>
+          <AdminRoute exact path='/admin-dashboard' component={AdminDashboard} />
           <Route exact path="/" component={Home} />
           <Route exact path='/ticketing' component={Ticketing} />
           <ProtectedRoute exact path="/profile" component={Profile} />
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,7 +12,7 @@ class Navbar extends React.Component {
     if (user.admin) {
       return(
       <Dropdown.Item>
-        <Link to = ''>ADMIN</Link>
+        <Link to = '/admin-dashboard'>ADMIN DASHBOARD</Link>
       </Dropdown.Item>
       )
     }
