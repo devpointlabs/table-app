125a3f34df4c280e1feed543095c0c1744c0e6d9
Maris Herrera <maris.lizete.herrera@gmail.com>
2019-01-14 11:02:16 -0700

    add form button in AdminDashboard.js,  create AdminDashboardStyle.js

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Button, } from 'semantic-ui-react';
+import { Button } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import { StyledSegment } from '../styles/AdminDashboardStyle'
 
 class AdminDashboard extends React.Component{
   render() {
     return (
       <div>
-        <Link to=''>
-          <Button inverted color='blue'>
-            + Add Event
-          </Button>
+        <Link to='/event-form'>
+          <StyledSegment basic>
+            <Button inverted color='blue'>
+              + Add Event
+            </Button>
+          </StyledSegment>
         </Link>
       </div>  
     )
---
+++ client/src/styles/AdminDashboardStyle.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { Segment } from 'semantic-ui-react';
+
+export const StyledSegment = styled(Segment)`
+margin-top: 600px !important;
+text-align: center !important;
+`
