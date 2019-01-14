1044ffa53a76f7fe87940f59d0251c1e5b647b7c
Maris Herrera <maris.lizete.herrera@gmail.com>
2019-01-14 11:27:59 -0700

    indent lines 5 and 6 AdminDashboard.js

--- client/src/styles/AdminDashboardStyle.js
+++ client/src/styles/AdminDashboardStyle.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { Segment } from 'semantic-ui-react';
 
 export const StyledSegment = styled(Segment)`
-margin-top: 75px !important;
-text-align: center !important;
+  margin-top: 75px !important;
+  text-align: center !important;
 `
 // for general use ^
