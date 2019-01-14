a564cd0269042c3fd55f1f62fa77d243ef09aa04
Colton Davis <colt822lavay@outlook.com>
2019-01-14 12:08:38 -0700

    Add Styling for Contacts, Fill Out Contacts.js

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,13 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Content, ContactsDiv, Head4, IMG, ImgWrap, Par } from '../styles/contactsStyle';
+import sbg from "./sbg.jpg";
 
-class Contact extends Component {
+class Contacts extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <Content>
+        <Head4>Contacts</Head4>
+        <ContactsDiv>
+          <ImgWrap>
+            <IMG src={sbg}/>
+            <IMG src={sbg}/>
+            <IMG src={sbg}/>
+            <IMG src={sbg}/>
+            <IMG src={sbg}/>
+            <IMG src={sbg}/>
+          </ImgWrap>
+          <div>
+            <Par>Joe LastName</Par>
+            <Par>Ian LastName</Par>
+            <Par>Matt LastName</Par>
+            <Par>Colton LastName</Par>
+            <Par>Maris LastName</Par>
+            <Par>Spencer LastName</Par>
+          </div>
+          <div>
+            <Par>Email@email.com</Par>
+            <Par>Email@email.com</Par>
+            <Par>Email@email.com</Par>
+            <Par>Email@email.com</Par>
+            <Par>Email@email.com</Par>
+            <Par>Email@email.com</Par>
+          </div>
+          <div>
+            <Par>555-555-5555</Par>
+            <Par>555-555-5555</Par>
+            <Par>555-555-5555</Par>
+            <Par>555-555-5555</Par>
+            <Par>555-555-5555</Par>
+            <Par>555-555-5555</Par>
+          </div>
+        </ContactsDiv>
+      </Content>
     )
   }
 }
 
-export default Contact
+export default Contacts;
+
---
+++ client/src/styles/contactsStyle.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Content= styled.div`
+  margin-top: 10%;
+  border-radius: 5px;
+`
+
+export const Head4 = styled.h4`
+  font-size: 3rem;
+  text-align: center;
+  background-color: #333;
+  width: 60%
+  margin: 0 auto
+`
+export const IMG = styled.img`
+  height: 50px
+  width: 50px;
+  margin-bottom: 5px;
+  border-radius: 5px;
+`
+export const ImgWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  max-height: 100%:
+`
+
+export const Par = styled.p`
+  height: 50px
+`
+
+export const ContactsDiv = styled.div`
+  display: flex; 
+  justify-content: space-between;
+  font-size: 1.5rem;
+  word-spacing: 10px
+  width: 60%;
+  margin: 0 auto;
+  padding-top: 1%;
+`
