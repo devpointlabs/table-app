0a35d5f3eec6bc5830a67a03f930dcd0cac4d331
Maris Herrera <maris.lizete.herrera@gmail.com>
2019-01-14 11:09:07 -0700

    add form button in AdminDashboard.js,  create AdminDashboardStyle.js

--- client/src/styles/AdminDashboardStyle.js
+++ client/src/styles/AdminDashboardStyle.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { Segment } from 'semantic-ui-react';
 
 export const StyledSegment = styled(Segment)`
-margin-top: 600px !important;
+margin-top: 75px !important;
 text-align: center !important;
 `
+// for general use ^
