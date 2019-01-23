523c83b6635f98c6ab9d9834fe4a9f4fe2a8b248
Joseph Ferro <joejoebeef@gmail.com>
2019-01-23 11:33:44 -0700

    re style profile view and edit view

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyledHeader, Line} from '../styles/generalitems';
+import { StyledHeader, Line, LeftDiv, RightDiv} from '../styles/generalitems';
 import axios from 'axios'
 
 class Contacts extends Component {
@@ -16,19 +16,19 @@ class Contacts extends Component {
     const { venue, } = this.state;
     return (
       <div style = {{ paddingTop: '190px', height: '100vh', display: 'flex' }}>
-        <div style = {{ textAlign: 'center', float: 'left', width: '50%'}}>
+        <LeftDiv>
           <StyledHeader large copperplate>contact</StyledHeader>
           <StyledHeader small gray>{venue.venue_name}</StyledHeader>
           <StyledHeader small gray>{venue.street_address}</StyledHeader>
           <StyledHeader small gray>{venue.city}</StyledHeader>
           <StyledHeader small gray>{venue.state}, {venue.zip_code}</StyledHeader>
-          <StyledHeader small gray>t: {venue.telephone}</StyledHeader>
-          <StyledHeader small gray><a href={"mailto:" + venue.email} >e: {venue.email}</a></StyledHeader>
-        </div>
+          <StyledHeader small gray>{venue.telephone}</StyledHeader>
+          <StyledHeader small gray><a href={"mailto:" + venue.email} >{venue.email}</a></StyledHeader>
+        </LeftDiv>
         <Line vertical />
-        <div style = {{ float: 'right', marginLeft: '40px', marginRight: '40px', paddingTop: '40px'}}>
+        <RightDiv>
           <StyledHeader small gray>{venue.about_us}</StyledHeader>
-        </div>
+        </RightDiv>
       </div>
     )
   } 
