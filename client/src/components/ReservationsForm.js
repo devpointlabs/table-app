cfa3e623bb2c896221a8ca7ec8ecca0a5a17b0cd
Matt Westover <matthew.westover@gmail.com>
2019-01-24 13:59:45 -0700

    add reservations form

---
+++ client/src/components/ReservationsForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Form } from 'semantic-ui-react'
+import { StyledButton, StyledHeader } from '../styles/Styles'
+
+class ReservationsForm extends React.Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default ReservationsForm;
--- client/src/components/VipTables.js
+++ client/src/components/VipTables.js
@@ -3,6 +3,7 @@ import { StyledHeader, StyledSegment, StyledButton } from '../styles/Styles';
 import { HeroWrapper, LayoutButton, } from '../styles/VipTableStyles'; 
 import { Container, Modal, Icon, Header, Image, Segment} from 'semantic-ui-react'
 import FloorPlan from '../images/Logo_Floorplan/Sky_SLC_Floor_Plan.png'
+import ReservationsForm from './ReservationsForm';
 
 class VipTables extends React.Component {
   state = { isOpen: false, }
@@ -40,6 +41,12 @@ class VipTables extends React.Component {
         </StyledHeader>
         <LayoutButton icon onClick={() => this.toggleOpen()}><Icon name='zoom in' /> View Table Layout</LayoutButton>
         { this.renderModal() }
+        <Segment basic style={{marginLeft: '75px', marginRight: '75px'}}>
+          <StyledHeader fSize='small'>
+            We give our patrons the opportunity to see world class shows in an intimate and eclectic setting. VIP Table Service allows you to experience these events by reserving your own private space, skipping the lines at the door, avoiding the lines at the bar, and most importantly, enjoying the show with the people you came to see it with. Whether you like to be the center of attention or enjoy the show privately from above, we’ve got the spot for you!
+          </StyledHeader>
+        </Segment>
+        <ReservationsForm />
       </StyledSegment>
       </>
     )
