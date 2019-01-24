79950991ecab3c71c0129c16f54d61992a46b541
Joseph Ferro <joejoebeef@gmail.com>
2019-01-24 16:18:16 -0700

    fix contact page style bugs

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -4,31 +4,38 @@ import { StyledHeader} from '../styles/Styles';
 import axios from 'axios'
 
 class Contacts extends Component {
-  state = { venue: [], }
+  state = { venue: [], text: '' }
   
   componentDidMount() {
     axios.get(`/api/settings/1`)
     .then( res => {
-      this.setState({ venue: res.data, })
+      this.setState({ venue: res.data,})
+      this.setState({ text: this.state.venue.about_us })
     })
   }
 
   render() {
-    const { venue, } = this.state;
+    const { venue, text } = this.state;
     return (
       <div style = {{ paddingTop: '190px', height: '100vh', display: 'flex' }}>
         <LeftDiv>
-          <StyledHeader large copperplate>contact</StyledHeader>
-          <StyledHeader small gray>{venue.venue_name}</StyledHeader>
-          <StyledHeader small gray>{venue.street_address}</StyledHeader>
-          <StyledHeader small gray>{venue.city}</StyledHeader>
-          <StyledHeader small gray>{venue.state}, {venue.zip_code}</StyledHeader>
-          <StyledHeader small gray>{venue.telephone}</StyledHeader>
-          <StyledHeader small gray><a href={"mailto:" + venue.email} >{venue.email}</a></StyledHeader>
+          <StyledHeader fSize = 'large' copperplate>contact</StyledHeader>
+          <StyledHeader fSize = 'small' gray defaultFont>{venue.venue_name}</StyledHeader>
+          <StyledHeader fSize = 'small' gray defaultFont>{venue.street_address}</StyledHeader>
+          <StyledHeader fSize = 'small' gray defaultFont>{venue.city}</StyledHeader>
+          <StyledHeader fSize = 'small' gray defaultFont>{venue.state}, {venue.zip_code}</StyledHeader>
+          <StyledHeader fSize = 'small' gray defaultFont>{venue.telephone}</StyledHeader>
+          <StyledHeader fSize = 'small' gray defaultFont><a href={"mailto:" + venue.email} >{venue.email}</a></StyledHeader>
         </LeftDiv>
         <Line vertical />
         <RightDiv>
-          <StyledHeader small gray>{venue.about_us}</StyledHeader>
+          <StyledHeader fSize = 'small' gray defaultFont spaced>
+          {
+            text.split("\n").map((i,key) => {
+            return <div key={key}>{i}</div>;
+            })
+          }
+          </StyledHeader>
         </RightDiv>
       </div>
     )
