6f4445234a02f144c036f24d94750a13c057c113
Matt Westover <matthew.westover@gmail.com>
2019-01-24 13:27:09 -0700

    modal toggles now

--- client/src/components/VipTables.js
+++ client/src/components/VipTables.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { StyledHeader, StyledSegment } from '../styles/Styles';
-import { HeroWrapper, } from '../styles/VipTableStyles'; 
-import { Container, Modal } from 'semantic-ui-react'
+import { StyledHeader, StyledSegment, StyledButton } from '../styles/Styles';
+import { HeroWrapper, LayoutButton, } from '../styles/VipTableStyles'; 
+import { Container, Modal, Icon, Header, Image, Segment} from 'semantic-ui-react'
 import FloorPlan from '../images/Logo_Floorplan/Sky_SLC_Floor_Plan.png'
 
 class VipTables extends React.Component {
+  state = { isOpen: false, }
 
+  toggleOpen = () => {
+    const {isOpen} = this.state;
+    this.setState({ isOpen: !isOpen});
+  }
+
+  renderModal = () => (
+    <Modal open={this.state.isOpen}>
+      <Modal.Header>
+        <Header as='h2' textAlign='center'>Table Layout</Header>
+      </Modal.Header>
+      <Segment basic textAlign='center'>
+        <Image src={FloorPlan} style={{margin: 'auto'}} />
+        <StyledButton onClick={() => this.toggleOpen()} >Close</StyledButton>
+      </Segment>
+    </Modal>
+  )
 
   render() {
     return(
@@ -21,6 +38,8 @@ class VipTables extends React.Component {
         <StyledHeader fSize='huge' underlined>
         VIP Table Reservations
         </StyledHeader>
+        <LayoutButton icon onClick={() => this.toggleOpen()}><Icon name='zoom in' /> View Table Layout</LayoutButton>
+        { this.renderModal() }
       </StyledSegment>
       </>
     )
