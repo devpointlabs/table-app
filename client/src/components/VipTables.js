a354613d447f82a63470721ac4f1d4f8d1f0d1e9
Matt Westover <matthew.westover@gmail.com>
2019-01-28 11:06:59 -0700

    user profile styling

--- client/src/components/VipTables.js
+++ client/src/components/VipTables.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyledHeader, StyledSegment, StyledButton } from '../styles/Styles';
 import { HeroWrapper, LayoutButton, } from '../styles/VipTableStyles'; 
-import { Container, Modal, Icon, Header, Image, Segment} from 'semantic-ui-react'
+import { Container, Modal, Icon, Image, Segment} from 'semantic-ui-react'
 import FloorPlan from '../images/Logo_Floorplan/Sky_SLC_Floor_Plan.png'
 import ReservationsForm from './ReservationsForm';
 
